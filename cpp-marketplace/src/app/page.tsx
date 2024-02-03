ca8fd15354ad97cf024bb58f103a60bdc47f291d
Samanyu <35706393+Samanyu24X@users.noreply.github.com>
2024-02-02 21:09:09 -0800

    created Navbar.tsx along with Navbar.css, added createListing folder to src

--- cpp-marketplace/src/app/page.tsx
+++ cpp-marketplace/src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// pages/index.tsx
 
-export default function Home() {
+import React from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
+import Navbar from '../../components/Navbar';
+
+const Home: React.FC = () => {
   return (
     <main className={styles.main}>
-
+      <Navbar />
+      {/* Other content of your app */}
     </main>
   );
-}
+};
+
+export default Home;
